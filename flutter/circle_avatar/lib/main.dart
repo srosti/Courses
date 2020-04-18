81dcc1f73f1639f7ce710b75652bea42918891ce
Shawn Rosti <srosti@cradlepoint.com>
2020-04-18 09:46:23 -0600

    Finished Flutter business card app

--- flutter/circle_avatar/lib/main.dart
+++ flutter/circle_avatar/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget> [
                 CircleAvatar(
                   radius: 50.0,
@@ -24,8 +25,63 @@ class MyApp extends StatelessWidget {
                       fontSize: 20,
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
-                    )
-                )
+                    ),
+                ),
+                Text(
+                  'Wizard',
+                  style: TextStyle(
+                    fontFamily: 'PressStart2P',
+                    fontSize: 10,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(
+                    color: Colors.teal.shade900,
+                  )
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 25
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                          'shawn.rosti@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20,
+                          )
+                      ),
+                    ),
+                  ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 25
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                        '208-953-12345',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                        )
+                    ),
+                  ),
+                ),
               ]
             )
           )
