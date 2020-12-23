c9b615f06d1d38b49b835a57d3ce1924c9de90bd
Shawn Rosti <srosti@mastodon.local.tld>
2020-12-23 10:44:49 -0700

    Example stateful widget

--- flutter/magic-8-ball-flutter-master/lib/main.dart
+++ flutter/magic-8-ball-flutter-master/lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+// Stateless widget
+class BallPage extends StatelessWidget {
+  const BallPage({ Key key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Magic 8 Ball'),
+      ),
+      body: Ball(),
+   );
+  }
+}
+
+// State widget
+class Ball extends StatefulWidget {
+  const Ball({ Key key }) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+            print('I got clicked next image #$ballNumber');
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
